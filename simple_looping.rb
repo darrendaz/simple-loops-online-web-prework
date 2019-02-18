9a570f0298ec569b8974cff91feba79aa01f29f1
Darren L <darren.lou@gmail.com>
2019-02-18 19:45:42 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,7 +33,8 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   count = 0
   while count < number_of_times
-  puts
+  puts phrase
+  count++
 end
   
 end
